54a6371a3ebe30b4abcb999058832f348af96dfe
Daniel Tello <dantello.319@gmail.com>
2021-09-16 21:06:16 -0500

    se arreglo typo en cors(access-control)

--- src/config/server.ts
+++ src/config/server.ts
@@ -32,7 +32,7 @@ export default class Server {
       this.app.use((req: Request, res: Response, next: NextFunction) => {
          res.header("Access-Control-Allow-Origin", "*");
          res.header(
-            "Acess-Control-Allow-Headers",
+            "Access-Control-Allow-Headers",
             "Content-Type, Authorization"
          );
          res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
